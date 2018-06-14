35bec09c910959afda2863dbd9ba31ac867d66cb
fabian bienz <fabian-bienz@users.noreply.github.com>
2018-06-14 09:51:57 +0200

    initial commit

--- src/Mediothek/Mediothek.java
+++ src/Mediothek/Mediothek.java
@@ -1,4 +1,18 @@
 package Mediothek;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Mediothek {
+	
+	private List kunden;
+	
+	public Mediothek() {
+		kunden = new ArrayList<Kunde>();
+	}
+	
+	
+	
+	
+	
 }
