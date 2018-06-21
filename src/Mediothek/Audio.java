ee230491da3a7e75416c88fc342305e77ab98dd0
fabian bienz <fabian-bienz@users.noreply.github.com>
2018-06-21 11:18:57 +0200

    added AudioBuilder

--- src/Mediothek/Audio.java
+++ src/Mediothek/Audio.java
@@ -9,7 +9,34 @@ public class Audio extends Medien {
 		this.lengthMin = lengthMin;
 	}
 	
+	public Audio(AudioBuilder builder) {
+		super.titel = builder._title;
+		this.lengthMin = builder._lenthMin;
+	}
 	public int getLengthMin() {
 		return lengthMin;
+	}
+	
+	public static AudioBuilder builder() {
+		return new AudioBuilder();
+	}
+	
+	public static class AudioBuilder {
+		private String _title;
+		private int _lenthMin;
+		
+		public AudioBuilder title(String _title) {
+			this._title = _title;
+			return this;
+		}
+		
+		public AudioBuilder lengthMin(int _lenthMin) {
+			this._lenthMin = _lenthMin;
+			return this;
+		}
+		
+		public Audio build() {
+			return new Audio(this);
+		}
 	}
 }
