7773f56279cf738d9753929f44f698b5db16fe7e
fabian bienz <fabian-bienz@users.noreply.github.com>
2018-06-21 08:23:14 +0200

    Id für Medien

--- src/Mediothek/Medien.java
+++ src/Mediothek/Medien.java
@@ -2,11 +2,14 @@ package Mediothek;
 
 public abstract class Medien {
 
-	private String titel;
-	
+	protected String titel;
+	private static int idCounter = 0;
+	protected int id;
 	
 	public Medien(String titel){
 		this.titel = titel;
+		idCounter++;
+		id = idCounter;
 	}
 
 }
