1aeb3dc554d7c6d6fd3f337644e3897d4a537f15
fabian bienz <fabian-bienz@users.noreply.github.com>
2018-06-21 08:34:51 +0200

    Ausleih Datum und String für Ausleiher, sowie Getter und Setter

--- src/Mediothek/Medien.java
+++ src/Mediothek/Medien.java
@@ -1,15 +1,43 @@
 package Mediothek;
 
+import java.util.Date;
+
 public abstract class Medien {
 
 	protected String titel;
 	private static int idCounter = 0;
 	protected int id;
+	private Date AusleihDatum;
+	private String Ausleiher = "";
 	
 	public Medien(String titel){
 		this.titel = titel;
 		idCounter++;
 		id = idCounter;
+		this.AusleihDatum = new Date();
 	}
-
+	
+	public String getTitel() {
+		return titel;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public Date getAusleihDatum() {
+		return AusleihDatum;
+	}
+	
+	public void setAusleihDatum(Date ausleihDatum) {
+		AusleihDatum = ausleihDatum;
+	}
+	
+	public String getAusleiher() {
+		return Ausleiher;
+	}
+	
+	public void setAusleiher(String ausleiher) {
+		Ausleiher = ausleiher;
+	}
 }
