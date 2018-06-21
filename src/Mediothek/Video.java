dfce5c47269543d5df5e987ec153575a2cc39942
fabian bienz <fabian-bienz@users.noreply.github.com>
2018-06-21 11:09:12 +0200

    added VideoBuilder

--- src/Mediothek/Video.java
+++ src/Mediothek/Video.java
@@ -9,7 +9,35 @@ public class Video extends Medien {
 		this.lengthMin = lengthMin;
 	}
 	
+	public Video(VideoBuilder builder) {
+		super.titel = builder._title;
+		this.lengthMin = builder._lengthMin;
+	}
+	
 	public int getLengthMin() {
 		return lengthMin;
+	}
+	
+	public static VideoBuilder builder() {
+		return new VideoBuilder();
+	}
+	
+	public static class VideoBuilder {
+		private String _title;
+		private int _lengthMin;
+		
+		public VideoBuilder title(String _title) {
+			this._title = _title;
+			return this;
+		}
+		
+		public VideoBuilder lengthMin(int _lengthMin) {
+			this._lengthMin = _lengthMin;
+			return this;
+		}
+		
+		public Video build() {
+			return new Video(this);
+		}
 	}
 }
