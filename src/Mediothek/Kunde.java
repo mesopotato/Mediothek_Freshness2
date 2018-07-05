3ce8153ef714bd9c3b10c5d195eab9aede9ef064
mesopotato <dittrich.yannick@gmail.com>
2018-07-05 11:43:58 +0200

    Du siech

--- src/Mediothek/Kunde.java
+++ src/Mediothek/Kunde.java
@@ -1,8 +1,92 @@
 package Mediothek;
+import java.util.*;
+import Mediothek.Buch.BuchBuilder;
 
 public class Kunde {
 	
-	public Kunde() {
+	public String name;
+	public boolean istMitglied = false ;
+	protected int id;
+	public Mitgliedschaft mitgliedschaft = null;
 	
+	
+	public Kunde(String name, int id) {
+		this.name = name;
+		this.id = id;
+		
 	}
+
+	public Date erstelleMitgliedschaft() {
+		
+		Mitgliedschaft mitgliedschaft = new Mitgliedschaft();
+		mitgliedschaft.renewMitgliedschaft();
+		return mitgliedschaft.getEndDatum();
+	}
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public boolean isIstMitglied() {
+		return istMitglied;
+	}
+
+
+	public void setIstMitglied(boolean istMitglied) {
+		this.istMitglied = istMitglied;
+	}
+
+
+	public Mitgliedschaft getMitgliedschaft() {
+		return mitgliedschaft;
+	}
+
+
+	public void setMitgliedschaft(Mitgliedschaft mitgliedschaft) {
+		this.mitgliedschaft = mitgliedschaft;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public Kunde(KundeBuilder builder) {
+		this.name = builder._name;
+		this.id = builder._id;
+	}
+	public static KundeBuilder builder() {
+		return new KundeBuilder();
+	}
+	
+	public static class KundeBuilder {
+		private String _name;
+		private int _id;
+		
+		public Kunde build() {
+			return new Kunde(this);
+		}
+		
+		public KundeBuilder name(String name) {
+			_name = name;
+			return this;
+		}
+
+		
+		public KundeBuilder id(int id) {
+			_id = id;
+			return this;
+		}
+	}
+	
 }
